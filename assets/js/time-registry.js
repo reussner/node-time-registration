d33909e42e8dca01d7cf07c412058391d4273fc4
Andreas Reußner <andreas.reussner@outlook.de>
2016-11-13 14:44:26 +0100

    Date field for time registry entry.
    Date is required for time registry. Centered X's on table.

--- assets/js/time-registry.js
+++ assets/js/time-registry.js
@@ -1,3 +1,16 @@
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+$(function(){
+  $('#timeRegistryEntryDate').fdatepicker({
+    initialDate: now,
+    format: 'mm-dd-yyyy',
+    disableDblClickSelection: true,
+    leftArrow:'<<',
+    rightArrow:'>>'
+  });
+});
+
 $(document).ready(function(){
   $('#chargeable').click(function(){
     $('#chargeableTime-container').toggle(this.checked);
