12c27ab2fa94be7aad62d662aaff2f06a825ed99
Unknown <reussner@ludwigseufert.de>
2016-11-28 18:48:05 +0100

    Forthcoming events calendar added to dashboard.

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -6,11 +6,79 @@
  */
 
 module.exports = {
-	get: function(req, res){
-		var options = {
-			title: 'Dashboard'
-		};
+  get: function (req, res) {
+    var month = getCurrentMonth();
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    var previousMonth = today.getMonth() - 1;
+    var calendarJson = getLeaveDateJson([]);
 
-		res.render('homepage', options);
-	}
+    var options = {
+      title: 'Dashboard',
+      calendar: month,
+      currentDate: today,
+      previousMonth: previousMonth,
+      calendarEvents: calendarJson
+    };
+
+    res.render('homepage', options);
+  }
+};
+
+function getCurrentMonth() {
+  var calendarStart = getCalendarStart(new Date());
+  // Show five weeks
+  var calendarEnd = getCalendarEnd(new Date());
+  var month = [];
+  var week = [];
+  while (calendarStart < calendarEnd) {
+    var day = {
+      number: calendarStart.getDate(),
+      date: calendarStart
+    };
+    week.push(day);
+    if (calendarStart.getDay() == 6) {
+      month.push(week);
+      week = [];
+    }
+    calendarStart = calendarStart.addDays(1);
+  }
+
+  return month;
+}
+
+function getCalendarStart(day) {
+  day.setHours(0, 0, 0, 0);
+  return new Date(day.valueOf()).addDays(-day.getDay());
+}
+
+function getCalendarEnd(day) {
+  day.setHours(0, 0, 0, 0);
+  var calendarStart = getCalendarStart(day);
+  return new Date(calendarStart.valueOf()).addDays(28);
+}
+
+function getLeaveDateJson(leaves) {
+  var jsonLeave = {};
+  for (var i = 0; i < leaves.length; i++) {
+    var start = leaves[i].startDate;
+    var end = leaves[i].endDate;
+    while (start <= end) {
+      if (jsonLeave.hasOwnProperty(start)) {
+        jsonLeave[start].push(leaves[i]);
+      } else {
+        jsonLeave[start] = [leaves[i]]
+      }
+
+      start = start.addDays(1);
+    }
+  }
+
+  return jsonLeave;
+}
+
+Date.prototype.addDays = function (days) {
+  var dat = new Date(this.valueOf());
+  dat.setDate(dat.getDate() + days);
+  return dat;
 };
