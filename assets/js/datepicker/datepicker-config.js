087dc3c8c308f69cbe50d441b474911222b94b2b
Unknown <reussner@ludwigseufert.de>
2016-11-29 19:13:09 +0100

    Set also time for leaves.

--- assets/js/datepicker/datepicker-config.js
+++ assets/js/datepicker/datepicker-config.js
@@ -3,29 +3,23 @@
  */
 var nowTemp = new Date();
 var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-var leaveStart = $('#start-date').fdatepicker({
-  format: 'mm-dd-yyyy',
+$('#start-date').fdatepicker({
+  initialDate: now,
+  format: 'yyyy-mm-dd hh:ii',
   leftArrow:'<<',
   rightArrow:'>>',
+  pickTime: true,
   onRender: function (date) {
     return date.valueOf() < now.valueOf() ? 'disabled' : '';
   }
-}).on('changeDate', function (ev) {
-  if (ev.date.valueOf() > leaveEnd.date.valueOf()) {
-    var newDate = new Date(ev.date);
-    newDate.setDate(newDate.getDate() + 1);
-    leaveEnd.update(newDate);
-  }
-  leaveStart.hide();
-  $('#end-date')[0].focus();
-}).data('datepicker');
-var leaveEnd = $('#end-date').fdatepicker({
-  format: 'mm-dd-yyyy',
-  leftArrow:'<<',
-  rightArrow:'>>',
+});
+$('#end-date').fdatepicker({
+  initialDate: now,
+  format: 'yyyy-mm-dd hh:ii',
+  leftArrow: '<<',
+  rightArrow: '>>',
+  pickTime: true,
   onRender: function (date) {
-    return date.valueOf() <= leaveStart.date.valueOf() ? 'disabled' : '';
+    return date.valueOf() < now.valueOf() ? 'disabled' : '';
   }
-}).on('changeDate', function (ev) {
-  leaveEnd.hide();
-}).data('datepicker');
+});
