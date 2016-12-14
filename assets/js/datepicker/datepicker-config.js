d3bb082f7833e227c3a2ed8fcfe3340475c0535c
Andreas Reußner <reussner@ludwigseufert.de>
2016-12-14 18:18:16 +0100

    Removed time insertion from leave application.
    Instead, just state full or half day.

--- assets/js/datepicker/datepicker-config.js
+++ assets/js/datepicker/datepicker-config.js
@@ -5,20 +5,18 @@ var nowTemp = new Date();
 var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 $('#start-date').fdatepicker({
   initialDate: now,
-  format: 'yyyy-mm-dd hh:ii',
+  format: 'yyyy-mm-dd',
   leftArrow:'<<',
   rightArrow:'>>',
-  pickTime: true,
   onRender: function (date) {
     return date.valueOf() < now.valueOf() ? 'disabled' : '';
   }
 });
 $('#end-date').fdatepicker({
   initialDate: now,
-  format: 'yyyy-mm-dd hh:ii',
+  format: 'yyyy-mm-dd',
   leftArrow: '<<',
   rightArrow: '>>',
-  pickTime: true,
   onRender: function (date) {
     return date.valueOf() < now.valueOf() ? 'disabled' : '';
   }
